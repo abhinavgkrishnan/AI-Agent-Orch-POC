f1fd84f45d67bb6798452e420a09a1ccbcce99b8
abhinavgkrishnan <abhinavgkrishnan@gmail.com>
2025-01-12 00:09:35 +0530

    Fix setupVite function call

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
+import { createServer } from "http";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
@@ -47,11 +48,14 @@ app.use((req, res, next) => {
     throw err;
   });
 
+  // Create the HTTP server
+  const server = createServer(app);
+
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
-    await setupVite(app);
+    await setupVite(app, server);
   } else {
     serveStatic(app);
   }
@@ -59,7 +63,7 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 4000
   // this serves both the API and the client
   const PORT = 4000;
-  app.listen(PORT, "0.0.0.0", () => {
+  server.listen(PORT, "0.0.0.0", () => {
     log(`serving on port ${PORT}`);
   });
 })();
